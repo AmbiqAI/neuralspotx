13b06cdf669f9e242108947cbf957d35c49a3a5d
Adam Page <apage224@gmail.com>
2026-06-12 13:59:36 -0500

    feat: support named linker profiles in M55 board modules
    Update the Apollo5-family Cortex-M55 board modules to honor the
    named NSX_LINKER_PROFILE selector instead of hardwiring
    NSX_LINKER_SCRIPT to the default SBL script.
    
    This keeps explicit NSX_LINKER_SCRIPT overrides authoritative while
    allowing the merged nsx-ambiq-sdk itcm profile to take effect for
    hpx and other neuralspotx consumers on apollo330mP_evb,
    apollo510_evb, apollo510b_evb, and apollo510dL_evb.

--- src/neuralspotx/boards/apollo510b_evb/board.cmake
+++ src/neuralspotx/boards/apollo510b_evb/board.cmake
@@ -27,11 +27,20 @@ set(NSX_CORE_DIR "${NSX_ROOT}/${_nsx_core_module_dir}")
 if(NSX_TOOLCHAIN_FAMILY STREQUAL "armclang")
     set(NSX_STARTUP_SOURCE "${NSX_CORE_DIR}/src/apollo510/armclang/startup_keil6.c")
     set(NSX_SYSTEM_SOURCE "${NSX_AMBIQSUITE_ROOT}/src/apollo510/system_apollo510.c")
-    set(NSX_LINKER_SCRIPT "${NSX_CORE_DIR}/src/apollo510b/armclang/linker_script_sbl.sct")
+    set(_nsx_linker_script_default "${NSX_CORE_DIR}/src/apollo510b/armclang/linker_script_sbl.sct")
+    set(_nsx_linker_script_itcm "${NSX_CORE_DIR}/src/apollo510b/armclang/linker_script_itcm_sbl.sct")
 else()
     set(NSX_STARTUP_SOURCE "${NSX_CORE_DIR}/src/apollo510/gcc/startup_gcc.c")
     set(NSX_SYSTEM_SOURCE "${NSX_AMBIQSUITE_ROOT}/src/apollo510/system_apollo510.c")
-    set(NSX_LINKER_SCRIPT "${NSX_CORE_DIR}/src/apollo510b/gcc/linker_script_sbl.ld")
+    set(_nsx_linker_script_default "${NSX_CORE_DIR}/src/apollo510b/gcc/linker_script_sbl.ld")
+    set(_nsx_linker_script_itcm "${NSX_CORE_DIR}/src/apollo510b/gcc/linker_script_itcm_sbl.ld")
+endif()
+
+if(NOT DEFINED NSX_LINKER_SCRIPT)
+    nsx_select_linker_script(
+        DEFAULT "${_nsx_linker_script_default}"
+        ITCM "${_nsx_linker_script_itcm}"
+    )
 endif()
 
 include("${NSX_CMAKE_DIR}/segger/socs/apollo5.cmake")
